dd88809b0d622bfe68edb6dbc53b04907d364d0c
Gabriel C <callaca_jose_1994@hotmail.com>
2020-01-17 22:40:02 -0500

    Vista formulario de libro de reclamos avance

---
+++ resources/views/web/claimbook.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.front')
+@section('title','Libro de reclamaciones')
+@section('content')
+<div class="container">
+	<div class="row mt-5">
+        Formulario libro de reclamaciones
+	</div>
+</div>
+
+@endsection
+
