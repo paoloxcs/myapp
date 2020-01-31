1aa915b67ef6e17f737e3c1115138403b16d0081
Gabriel C <callaca_jose_1994@hotmail.com>
2020-01-30 23:07:18 -0500

    se corregiò valor por defecto para el campo status de la tabla categories

--- database/migrations/2019_10_08_134556_create_categories_table.php
+++ database/migrations/2019_10_08_134556_create_categories_table.php
@@ -19,7 +19,7 @@ class CreateCategoriesTable extends Migration
             $table->string('slug');
             $table->text('description');
             $table->string('url_image', 45);
-            $table->boolean('status');
+            $table->boolean('status')->default(1);
 
             //$table->timestamps();
         });
